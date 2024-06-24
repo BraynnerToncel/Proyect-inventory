f1cc5569bd194754398bac11fc1025603afa8230
unknown <braynnert31@gmal.com>
2024-06-24 11:25:53 -0500

    connection modification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigAsync } from './data/database-config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getEnvPath } from 'common/helper/env.helper';
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
+const envFilePath: string = getEnvPath(`${__dirname}/../common/envs`);
+console.log(envFilePath);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    EventEmitterModule.forRoot({ delimiter: '.' }),
+
+    TypeOrmModule.forRootAsync(TypeOrmConfigAsync),
+    ServeStaticModule.forRoot({
+      serveRoot: '/static',
+      rootPath: process.env.STORE_FILES_PATH,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
